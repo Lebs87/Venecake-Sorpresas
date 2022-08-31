c0c4fb615e1558cadae8a26e346769496925c1b8
lebs87 <lebs87@gmail.com>
2022-08-31 14:53:50 -0300

    apertura de repositorio para VenecakeSorpresas

---
+++ README.md
@@ -0,0 +1,5 @@
+Creación de Aplicación Web para Vene Cake Sorpresa
+
+1 Etapa Desarrollo de HTML
+2 Etapa Desarrollo de CSS
+3 Etapa Desarrollo de JAVASCRIPT
---
+++ js/index.js
@@ -0,0 +1,9 @@
+let nombre = "Luis"
+
+//alert ("hola a todos")
+let nombreCompleto = prompt("Por favor, ingresa tu nombre")
+//let confirmar = confirm("quieres un descanso")
+
+console.log ("el nombre del usuario es", nombreCompleto)
+
+alert ("hola!", nombreCompleto)
